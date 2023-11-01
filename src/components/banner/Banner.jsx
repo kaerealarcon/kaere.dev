5905ed0f4d61ac23139320de00ceebb3e6ce1666
Kaere <kaere.contato@gmail.com>
2023-11-01 11:28:09 -0300

    fetch: portfolio v0.2

--- src/components/banner/Banner.jsx
+++
@@ -1,9 +0,0 @@
-export const Banner = () => {
-    return (
-        <>
-        <h1>UI Designer e Front-end</h1>
-        
-        <p>Sou especialista em interfaces funcionais e que potencializam o seu negócio desde o MVP até o produto final.</p>
-        </>
-    )
-}
--- src/components/button/Button.jsx
+++
@@ -1,5 +0,0 @@
-export const Button = () => {
-    return (
-        <a href="#">Ver todos os projetos</a>
-    )
-}
